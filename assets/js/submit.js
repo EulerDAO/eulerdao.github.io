d45aefad4ef269d17733aa3e314fdc3e0e827bb4
vang1ong7ang <vang1ong7ang@outlook.com>
2021-06-28 08:07:21 +0000

    dev

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,8 +1,8 @@
 class Submit {
     constructor() {
         this.args = new URLSearchParams(window.location.search);
-        const resp = fetch(`/problems/data/${this.args.get('id')}/solution.txt`).then(v => v.text()).then(content => {
-            document.getElementById('submit-title').innerText = `Problem ${this.args.get('id')}`;
+        fetch(`/problems/data/${this.args.get('id')}/solution.txt`).then(v => v.text()).then(content => {
+            document.getElementById('title').innerText = `Problem ${this.args.get('id')}`;
             document.getElementById('code').value = content;
         })
     }
