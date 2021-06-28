20f4ba43030f4711f67a705fb6bbec3801d4f230
vang1ong7ang <vang1ong7ang@outlook.com>
2021-06-28 08:00:07 +0000

    dev

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,15 +1,11 @@
 class Submit {
     constructor() {
         this.args = new URLSearchParams(window.location.search);
+        const resp = await fetch(`/problems/data/${this.args.get('id')}/solution.txt`);
+        const content = await resp.text();
         document.getElementById('submit-title').innerText = `Problem ${this.args.get('id')}`;
+        document.getElementById('code').value = content;
     }
-
-    async render() {
-        const x = await fetch(`/problems/data/${this.args.get('id')}/solution.txt`);
-        const content = await x.text();
-        document.getElementById('submit-code').value = content;
-    }
-
     submit() {
         alert('submit')
     }
@@ -18,8 +14,4 @@ class Submit {
 
 window.addEventListener('load', () => {
     window.submit = new Submit();
-    window.submit.render();
-    document.getElementById('submit-button').addEventListener('click', ()=>{
-        window.submit.submit();
-    })
 })
--- submit.md
+++ submit.md
@@ -1,7 +1,9 @@
 <div id="submit" style="width: 100%;">
-    <h3 id="submit-title" style='text-align: center'> Problem X </h3>
-    <textarea id="submit-code" style='width: 100%; min-height: 25vh'> </textarea>
-    <button id="submit-button"> Submit </button>
+    <h3 id="title" style='text-align: center'>
+    <script>console.log(this)</script>
+    </h3>
+    <textarea id="code" style='width: 100%; min-height: 25vh'> </textarea>
+    <button onclick="window.submit.submit()"> Submit Solution </button>
 </div>
 
 <script src="/assets/js/submit.js"></script>
