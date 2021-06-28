a1e9b66d02c59b86e9076a2e869650eb1cefcf81
ds <ds@ds.ds>
2021-06-28 18:51:02 +0800

    add submit logic

--- assets/js/dist/problem.dev.js
+++ assets/js/dist/problem.dev.js
@@ -14,7 +14,6 @@ function () {
 
     this.args = new URLSearchParams(window.location.search);
     document.getElementById('content').src = "/problems/".concat(this.args.get('id'));
-    document.getElementById('submit').href = "/submit?id=".concat(this.args.get('id'));
   }
 
   _createClass(Problem, [{
@@ -22,6 +21,52 @@ function () {
     value: function resize() {
       var obj = document.getElementById('content');
       obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+    }
+  }, {
+    key: "submit",
+    value: function submit() {
+      var pool, signer, bytecode, digest, abi, contract;
+      return regeneratorRuntime.async(function submit$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              pool = '0x64f4FD397E2Ca009912ef7C53219eAB4D7A74157'; //todo
+
+              signer = window.wallet.signer;
+
+              if (!(signer === null)) {
+                _context.next = 5;
+                break;
+              }
+
+              alert("Connect wallect first!");
+              return _context.abrupt("return");
+
+            case 5:
+              bytecode = document.getElementById('code').value;
+
+              if (!bytecode.startsWith('0x')) {
+                document.getElementById('code').value = '0x' + document.getElementById('code').value;
+                bytecode = document.getElementById('code').value;
+              }
+
+              try {
+                digest = window.ethers.utils.keccak256(bytecode);
+              } catch (_unused) {
+                alert('invald bytecode');
+              }
+
+              abi = ['function lock_challenge(uint256 id) public payable'];
+              contract = new window.ethers.Contract(pool, abi, signer);
+              _context.next = 12;
+              return regeneratorRuntime.awrap(contract.lock_challenge(digest));
+
+            case 12:
+            case "end":
+              return _context.stop();
+          }
+        }
+      });
     }
   }]);
 
--- assets/js/problem.js
+++ assets/js/problem.js
@@ -7,6 +7,31 @@ class Problem {
         const obj = document.getElementById('content');
         obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
     }
+    async submit() {
+        const pool = '0x64f4FD397E2Ca009912ef7C53219eAB4D7A74157'; //todo
+        const signer = window.wallet.signer;
+        if (signer === null) {
+            alert("Connect wallect first!");
+            return;
+        }
+
+        var bytecode = document.getElementById('code').value;
+        var digest;
+        if (!bytecode.startsWith('0x')) {
+            document.getElementById('code').value = '0x' + document.getElementById('code').value
+            bytecode = document.getElementById('code').value;
+        }
+        try{
+            digest = window.ethers.utils.keccak256(bytecode);
+        } catch {
+            alert('invald bytecode')
+        }
+        const abi = [
+            'function lock_challenge(uint256 id) public payable',
+        ]
+        const contract = new window.ethers.Contract(pool, abi, signer);
+        await contract.lock_challenge(digest);
+    }
 }
 
 window.addEventListener('load', () => {
