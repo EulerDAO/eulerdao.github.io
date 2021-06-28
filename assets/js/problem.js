782d58bfe1222ba60a7bddb016f6c1585e96f4d3
vang1ong7ang <vang1ong7ang@outlook.com>
2021-06-28 04:25:25 +0000

    dev

--- assets/js/problem.js
+++ assets/js/problem.js
@@ -3,6 +3,10 @@ class Problem {
         this.args = new URLSearchParams(window.location.search);
         document.getElementById('content').src = `/problems/${this.args.get('id')}`
     }
+    resize() {
+        const obj = document.getElementById('content');
+        obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+    }
 }
 
 window.addEventListener('load', () => {
--- problem.md
+++ problem.md
@@ -1,2 +1,2 @@
-<iframe id="content"></iframe>
+<iframe id="content" frameborder="0" scrolling="no" onload="window.problem.resize()" style="width: 100%;"></iframe>
 <script src="/assets/js/problem.js"></script>
