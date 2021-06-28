fddfd19bf49dc8543264604df852a536854eafd4
vang1ong7ang <vang1ong7ang@outlook.com>
2021-06-28 12:04:44 +0000

    dev

--- assets/js/problem.js
+++ assets/js/problem.js
@@ -7,42 +7,36 @@ class Problem {
         const obj = document.getElementById('content');
         obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
     }
-    refine(event) {
-        var bytecode = event.clipboardData.getData('text');
-        try {
-            console.log(bytecode)
-            bytecode = JSON.parse(bytecode).object;
-            console.log(bytecode)
-        } catch {
-        }
-        console.log(bytecode)
-        if ((/^[0-9A-Fa-f]+$/g).test(bytecode)) {
-            bytecode = '0x' + bytecode;
-        }
-        console.log(bytecode)
-        document.getElementById('code').value = bytecode;
-        event.preventDefault();
-    }
     async submit() {
-        const pool = '0x64f4FD397E2Ca009912ef7C53219eAB4D7A74157'; // TODO
         const signer = window.wallet.signer;
         if (signer === null) {
             alert("Connect wallect first!");
             return;
         }
 
-        var bytecode = document.getElementById('code').value;
+        let bytecode = document.getElementById('code').value;
+        try {
+            bytecode = JSON.parse(bytecode).object;
+        } catch {
+        }
+        if ((/^[0-9A-Fa-f]+$/g).test(bytecode)) {
+            bytecode = '0x' + bytecode;
+        }
+
+        const ed = '0x64f4FD397E2Ca009912ef7C53219eAB4D7A74157'; // TODO
         var digest;
         try {
             digest = window.ethers.utils.keccak256(bytecode);
         } catch {
             alert('invald bytecode')
+            return;
         }
         const abi = [
             'function lock_challenge(uint256 id) public payable',
         ]
-        const contract = new window.ethers.Contract(pool, abi, signer);
+        const contract = new window.ethers.Contract(ed, abi, signer);
         await contract.lock_challenge(digest);
+        window.open(`/solution?id=${digest}`, '_blank');
     }
 }
 
