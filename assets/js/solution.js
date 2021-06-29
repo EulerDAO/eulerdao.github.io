7bb8a3f79c9f59aeecc6ebe153a217da48242bf6
ds <ds@ds.ds>
2021-06-29 15:55:54 +0800

    dev

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,16 +1,24 @@
 class Solution {
     constructor() {
+        this.ed = '0xC3a65484e3D59689B318fB23c210a079873CFfbB'; // TODO
         this.args = new URLSearchParams(window.location.search);
         this.digest = this.args.get('id');
+        const cachedCode = window.localStorage.getItem(this.digest);
+        document.getElementById('bytecode').value = cachedCode;
+        document.getElementById('bytecode').placeholder = 'Input your solidity bytecode here.\nIt looks like 0x1234567890abcdef and must start with "0x".\nThis must be as same as the one you submited on problem page.';
+        this.address = window.ethers.utils.keccak256(
+            window.ethers.utils.defaultAbiCoder.encode(
+                ["bytes1", "address", "bytes32", "bytes32"],
+                ["0xff", this.ed, "0x0000000000000000000000000000000000000000000000000000000000000000", this.digest])
+        );
     }
     async render() {
         try{
-            const ed = '0xC3a65484e3D59689B318fB23c210a079873CFfbB'; // TODO
             const abi = [
                 'function targets(uint256) external view returns(uint256)',
                 'function ownerOf(uint256 id) external view returns (address)',
             ]
-            const contract = new window.ethers.Contract(ed, abi, window.wallet.signer);
+            const contract = new window.ethers.Contract(this.ed, abi, window.wallet.signer);
             this.id = await contract.targets(this.digest);
             this.owner = await contract.ownerOf(this.digest);
         } catch{}
@@ -20,16 +28,13 @@ class Solution {
             console.log(window.solution);
             const chainid = await window.wallet.signer.getChainId();
             const network = window.ethers.providers.getNetwork(chainid);
-            document.getElementById('etherscan').href = `https://${network.name === 'homestead' ? '' : network.name + '.'}etherscan.io/address/${this.digest}`
+            document.getElementById('etherscan').href = `https://${network.name === 'homestead' ? '' : network.name + '.'}etherscan.io/address/${this.address}`
             document.getElementById('etherscan').style.visibility = 'visible';
         }
         if (this.owner !== undefined) {
             document.getElementById('problem').href = `/problems/${this.id}`;
             document.getElementById('problem').style.visibility = 'visible';
         }
-        const cachedCode = window.localStorage.getItem(this.digest);
-        document.getElementById('bytecode').value = cachedCode;
-        document.getElementById('bytecode').placeholder = 'Input your solidity bytecode here.\nIt looks like 0x1234567890abcdef and must start with "0x".\nThis must be as same as the one you submited on problem page.';
     }
 }
 
