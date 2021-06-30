08a3afe6a6d379ac5a9080256db649f3dea32bd6
ds <ds@ds.ds>
2021-06-30 16:17:06 +0800

    refine

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -9,8 +9,10 @@ class Solution {
         if (!window.wallet.signer) {
             return;
         }
+        const chainid = await window.wallet.signer.getChainId();
+        const network = window.ethers.providers.getNetwork(chainid);
         const ctx = {};
-        const contract = new window.ethers.Contract('0xC3a65484e3D59689B318fB23c210a079873CFfbB', ['function targets(uint256) external view returns(uint256)', 'function scores(uint256) external view returns(uint256)', 'function timestamps(uint256) external view returns(uint256)', 'function challengers(uint256) external view returns(address)', 'function ownerOf(uint256) external view returns (address)', 'function submit_code(bytes memory code) external'], window.wallet.signer);
+        const contract = new window.ethers.Contract('0xC3a65484e3D59689B318fB23c210a079873CFfbB', ['function revoke(uint256 id) external payable', 'function challenge(uint256 id, bytes calldata i) external payable', 'function lock_challenge(uint256 id) public payable', 'function targets(uint256) external view returns(uint256)', 'function compete(uint256 id, uint256 score) external payable', 'function scores(uint256) external view returns(uint256)', 'function timestamps(uint256) external view returns(uint256)', 'function challengers(uint256) external view returns(address)', 'function ownerOf(uint256) external view returns (address)', 'function submit_code(bytes memory code) external'], window.wallet.signer);
         try {
             ctx.target = await contract.targets(digest);
         } catch { }
@@ -29,20 +31,128 @@ class Solution {
         try {
             ctx.code = await contract.provider.getCode(address);
         } catch { }
+        try {
+            ctx.me = await window.wallet.signer.getAddress();
+        } catch { }
+
+        var timePassed = Math.max(0, Math.floor(Date.now() / 1000) - ctx.timestamp.toNumber() - 15);
+        var fee = window.ethers.BigNumber.from('0xffffffffffffffffffffffffffffffff').div(window.ethers.constants.One.shl(Math.floor(timePassed / 8)));
 
         if (ctx.owner === undefined) {
             document.getElementById('code').innerText = 'Solution not Found';
             return;
         }
 
-        document.getElementById('problem').href = `/problem?id=${ctx.target}`;
-        document.getElementById('problem').style.visibility = 'visible';
-        document.getElementById('td-score').innerText = ctx.score;
-        document.getElementById('td-owner').innerText = ctx.owner;
-        document.getElementById('td-challenger').innerText = ctx.challenger;
-        console.log(ctx.timestamp.toNumber(), typeof(ctx.timestamp));
-        document.getElementById('td-timestamp').innerText = new Date(ctx.timestamp.toNumber()*1000).toLocaleString();
         document.getElementById('info').style.visibility = 'visible';
+
+        {
+            function showCD() {
+                const fullCD = 1024;
+                if (timePassed < fullCD) {
+                    timePassed = Math.max(0, Math.floor(Date.now() / 1000) - ctx.timestamp.toNumber() - 15);
+                    fee = window.ethers.BigNumber.from('0xffffffffffffffffffffffffffffffff').div(window.ethers.constants.One.shl(Math.floor(timePassed / 8)));
+                    document.getElementById('td-timestamp').innerText = `Colling Down...[${fullCD - timePassed}]; You'll need to pay ${ethers.utils.formatEther(fee)} ether to skip CD`;
+                    setTimeout(function () {
+                        showCD();
+                    }, 500);
+                } else {
+                    document.getElementById('td-timestamp').innerText = `Free`;
+                }
+            }
+            showCD()
+        }
+        if (ctx.challenger !== ctx.me) {
+            const a = document.createElement('a');
+            a.href = `https://${network.name === 'homestead' ? '' : network.name + '.'}etherscan.io/address/${ctx.challenger}`;
+            a.innerText = `${ctx.challenger}`;
+            a.target = '_blank';
+            const button = document.createElement('button');
+            if (ctx.me == ctx.owner) {
+                button.innerText = 'start revoke'
+            } else {
+                button.innerText = 'start challenge'
+            }
+            button.onclick = async () => {
+                try {
+                    const resp = await contract.lock_challenge(this.args.get('id'), { value: fee });
+                    await resp.wait();
+                    window.location.reload();
+                } catch (e) {
+                    console.log(e);
+                    if (e.toString().indexOf('insufficient funds') >= 0) {
+                        alert('insufficient funds');
+                    }
+                }
+            };
+            document.getElementById('td-challenger').innerHTML = '';
+            document.getElementById('td-challenger').appendChild(a);
+            document.getElementById('td-challenger').appendChild(button);
+        } else {
+            const button = document.createElement('button');
+            let input;
+            if (ctx.me == ctx.owner) {
+                button.innerText = 'revoke';
+                button.onclick = async () => {
+                    const resp = await contract.revoke(this.args.get('id'));
+                    await resp.wait();
+                    window.location.reload();
+                };
+            } else {
+                input = document.createElement('input');
+                input.placeholder = 'abi encoded input';
+                button.innerText = 'challenge';
+                button.onclick = async () => {
+                    const resp = await contract.challenge(this.args.get('id'), input.value);
+                    await resp.wait();
+                    window.location.reload();
+                };
+
+            }
+            document.getElementById('td-challenger').innerHTML = '';
+            if (input) {
+                document.getElementById('td-challenger').appendChild(input);
+            }
+            document.getElementById('td-challenger').appendChild(button);
+
+        }
+        {
+            const a = document.createElement('a');
+            a.href = `/problem?id=${ctx.target}`;
+            a.innerText = `Problem ${ctx.target}`;
+            a.target = '_blank';
+            document.getElementById('td-problem').innerHTML = '';
+            document.getElementById('td-problem').appendChild(a);
+        }
+        {
+            const a = document.createElement('a');
+            a.href = `https://${network.name === 'homestead' ? '' : network.name + '.'}etherscan.io/address/${ctx.owner}`;
+            a.innerText = `${ctx.owner}`;
+            a.target = '_blank';
+            document.getElementById('td-owner').innerHTML = '';
+            document.getElementById('td-owner').appendChild(a);
+        }
+        if (ctx.score.eq(0)) {
+            if (ctx.owner === ctx.me) {
+                const input = document.createElement('input');
+                const button = document.createElement('button');
+                input.placeholder = 'input your score';
+                button.innerText = 'Compete';
+                button.onclick = async () => {
+                    const score = input.value;
+                    const val = window.ethers.BigNumber.from('100000000000').mul(window.ethers.BigNumber.from(input.value)).add(window.ethers.constants.WeiPerEther);
+                    const resp = await contract.compete(this.args.get('id'), score, { value: val.toString() });
+                    await resp.wait();
+                    window.location.reload();
+                };
+                document.getElementById('td-score').innerHTML = ``;
+                document.getElementById('td-score').appendChild(input);
+                document.getElementById('td-score').appendChild(button);
+            } else {
+                document.getElementById('td-score').innerText = `❌[NO SCORE]`;
+            }
+        } else {
+            document.getElementById('td-score').innerText = `✔️[${ctx.score}]`;
+        }
 
 
         switch (ctx.code) {
@@ -71,14 +181,12 @@ class Solution {
                     }
                     const resp = await contract.submit_code(bytecode);
                     await resp.wait();
-                    window.location.reload()
+                    window.location.reload();
                 };
                 document.getElementById('code').appendChild(button);
                 break;
             default:
                 document.getElementById('code').innerHTML = '';
-                const chainid = await window.wallet.signer.getChainId();
-                const network = window.ethers.providers.getNetwork(chainid);
                 const h1 = document.createElement('h1');
                 h1.style.textAlign = 'center';
                 h1.style.color = 'green';
@@ -92,62 +200,6 @@ class Solution {
                 document.getElementById('code').appendChild(a);
                 break;
         }
-
-
-        // entered or not not entered
-        if (ctx.score == 0) {
-            document.getElementById('noenter').style.visibility = 'visible';
-            return;
-        }
-        document.getElementById('entered').style.visibility = 'visible';
-        document.getElementById('showscore').innerText = `Score: ${ctx.score}`
-
-        // has challenger or not
-        const thisAddr = await window.wallet.signer.getAddress();
-        if (!ctx.challenger.eq(window.ethers.BigNumber.from(thisAddr))) {
-            document.getElementById('lock').style.visibility = 'visible';
-            if (thisAddr === ctx.owner) {
-                document.getElementById('lock').innerText = 'prepare to revoke'
-            } else {
-                document.getElementById('lock').innerText = 'to be the challenger'
-            }
-        } else {
-            if (thisAddr == ctx.owner) {
-                document.getElementById('revoke').style.visibility = 'visible';
-            } else {
-                document.getElementById('challenge').style.visibility = 'visible';
-            }
-        }
-    }
-    async compete() {
-        const abi = [
-            'function compete(uint256 id, uint256 score) external payable',
-        ]
-        const contract = new window.ethers.Contract(this.ed, abi, window.wallet.signer);
-        const score = document.getElementById('score').value;
-        console.log('owner', this.owner);
-        await contract.compete(this.digest, score, { value: '2000000000000000000' });
-    }
-    async lock() {
-        const abi = [
-            'function lock_challenge(uint256 id) public payable',
-        ]
-        const contract = new window.ethers.Contract(this.ed, abi, window.wallet.signer);
-        await contract.lock_challenge(this.digest);
-    }
-    async challenge() {
-        const abi = [
-            'function challenge(uint256 id, bytes calldata i) external payable',
-        ]
-        const contract = new window.ethers.Contract(this.ed, abi, window.wallet.signer);
-        await contract.challenge(this.digest, '0x11');
-    }
-    async revoke() {
-        const abi = [
-            'function revoke(uint256 id) external payable ',
-        ]
-        const contract = new window.ethers.Contract(this.ed, abi, window.wallet.signer);
-        await contract.revoke(this.digest);
     }
 }
 
--- solution.md
+++ solution.md
@@ -1,10 +1,13 @@
 <div id="code">Loading ...</div>
-<a id='problem' style="display: block; text-align: center; visibility: hidden">Go to Problem</a>
 <hr>
 <table id='info' style="visibility: hidden">
   <tr>
     <th>Score</th>
     <td id='td-score'>0</td>
+  </tr>
+  <tr>
+    <th>Problem</th>
+    <td id='td-problem'>0</td>
   </tr>
   <tr>
     <th>Owner</th>
@@ -15,8 +18,8 @@
     <td id='td-challenger'>0x0000000000000000000000000000000000000000</td>
   </tr>
   <tr>
-    <th>Chanllenge Time</th>
-    <td id='td-timestamp'>1970-01-01 00:00:00</td>
+    <th>CD (seconds)</th>
+    <td id='td-timestamp'>20</td>
   </tr>
   <tr>
     <th>Rank</th>
@@ -25,18 +28,4 @@
 </table>
 
 
-<div id='noenter' style="visibility: hidden">
-    <h3  style="display: block; text-align: center">Not Entered</h3>
-    <div style='align-items: center;/* width: 50%; */margin: auto auto;display: flex;justify-content: center;'><label styple="vertical-align:middle;display:inline-block; ">Your Score:</label><input style='text-align:center;vertical-align:middle;' id='score' /></div>
-    <a id='compete' onclick='window.solution.compete()' style="display: block; text-align: center">Compete</a>
-</div>
-<div id='entered' style="visibility: hidden">
-    <h3 style="display: block; text-align: center">Entered</h3>
-    <h3 id='showscore'  style="display: block; text-align: center">Score: x</h3>
-    <h3 id='rank' style="display: block; text-align: center; visibility: hidden">Rank: x</h3>
-    <h3 id='challenger' style="display: block; text-align: center; visibility: hidden">Challenger: x</h3>
-    <a id='lock' onclick='window.solution.lock()' style="display: block; text-align: center; visibility: hidden" >Lock</a>
-    <a id='challenge' onclick='window.solution.challenge()' style="display: block; text-align: center;visibility: hidden"  >Challenge</a>
-    <a id='revoke' onclick='window.solution.revoke()' style="display: block; text-align: center;visibility: hidden" >Revoke</a>
-</div>
 <script src="/assets/js/solution.js"></script>
