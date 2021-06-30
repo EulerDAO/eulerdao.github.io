cde462c311ffd811d149ad0542dff8a790ad2785
vang1ong7ang <vang1ong7ang@outlook.com>
2021-06-30 08:59:51 +0000

    dev

--- assets/js/problem.js
+++ assets/js/problem.js
@@ -30,6 +30,7 @@ class Problem {
         }
         try {
             await contract.ownerOf(digest);
+            alert("solution has been submited")
             window.open(`/solution?id=${digest}`, '_blank');
             return;
         } catch {
